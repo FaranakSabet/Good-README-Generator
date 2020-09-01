553662291dc049b90d28f3f4c323288dcf3766d1
faranak Sabet <faranaksabet@yahoo.com>
2020-09-01 13:25:01 -0700

    another commit

---
+++ README.md
@@ -0,0 +1,47 @@
+## Title
+
+read me generato
+
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+## Description
+
+this is a test
+
+## Table of Contents
+
+- [Title](#title)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+instal info goes here
+
+## Usage
+
+use info goes here
+
+## License
+
+This project is covered under the ISC license.
+
+## Contributing
+
+none
+
+## Tests
+
+test goes here
+
+## Questions
+
+For questions please reach me at my [email](mailto:faranaksabet@yahoo.com) or you can visit my [GitHub profile](https://github.com/faranaksabet) for additional resources.
+
+The link to my video:
+https://drive.google.com/file/d/18R0wpZej2r0gsFOP0SE31mn-dntflMS4/view
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var inquirer = require("inquirer");
-var fs = require('fs');
+var fs = require("fs");
 
 console.log("Hi, welcome to the README.md generator");
 
@@ -9,7 +9,7 @@ var questions = [
     name: "title",
     message: "What's the title of your project? ",
     validate: function (value) {
-      if (value != '') {
+      if (value != "") {
         return true;
       }
       return "Please enter a valid title";
@@ -18,27 +18,27 @@ var questions = [
   {
     type: "input",
     name: "description",
-    message: "Write a brief description of your project: "
+    message: "Write a brief description of your project: ",
   },
   {
     type: "input",
     name: "installation",
-    message: "Write the installation instructions for your project: "
+    message: "Write the installation instructions for your project: ",
   },
   {
     type: "input",
     name: "usage",
-    message: "Write down the usage information for your project: "
+    message: "Write down the usage information for your project: ",
   },
   {
     type: "input",
     name: "contribution",
-    message: "Write down the contribution guidelines for your project: "
+    message: "Write down the contribution guidelines for your project: ",
   },
   {
     type: "input",
     name: "test",
-    message: "Write down the test instructions for your project: "
+    message: "Write down the test instructions for your project: ",
   },
   {
     type: "list",
@@ -54,7 +54,7 @@ var questions = [
     name: "github",
     message: "What's your GitHub username? ",
     validate: function (value) {
-      if (value != '') {
+      if (value != "") {
         return true;
       }
       return "Please enter a valid username";
@@ -65,30 +65,32 @@ var questions = [
     name: "email",
     message: "What's your email address? ",
     validate: function (value) {
-      var pass = value.match(
-        /^.+@[a-z]+\.[^.1-9]+$/i
-      );
-      if (pass)
-        return true;
+      var pass = value.match(/^.+@[a-z]+\.[^.1-9]+$/i);
+      if (pass) return true;
       return "Please enter a valid email address";
     },
-  }
+  },
 ];
 
 inquirer.prompt(questions).then((answers) => {
-  console.log(answers);
-  fs.open('README.md', 'w', function (err, file) {
+  //console.log(answers);
+
+  fs.open("README.md", "w", function (err, file) {
     if (err) throw err;
 
     let content = `## Title\n${answers.title}\n\n`;
-    if (answers.license == 'mit')
-      content += '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n';
-    else if (answers.license == 'isc')
-      content += '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)\n\n';
-    else if (answers.license == 'bsd3')
-      content += '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n\n';
+    if (answers.license == "mit")
+      content +=
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n";
+    else if (answers.license == "isc")
+      content +=
+        "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)\n\n";
+    else if (answers.license == "bsd3")
+      content +=
+        "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n\n";
     else
-      content += '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n';
+      content +=
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n";
 
     content += `## Description\n${answers.description}\n\n`;
 
@@ -97,42 +99,20 @@ inquirer.prompt(questions).then((answers) => {
     content += `## Installation\n${answers.installation}\n\n`;
     content += `## Usage\n${answers.usage}\n\n`;
 
-
-    // content += `## License\n${answers.usage}\n\n`;
-    // a notice is added to the section of the README entitled License that explains which license the application is covered under
-
+    content += `## License\nThis project is covered under the ${answers.license.toUpperCase()} license.\n\n`;
 
     content += `## Contributing\n${answers.contribution}\n\n`;
     content += `## Tests\n${answers.test}\n\n`;
-    content += `## Questions\nFor questions please reach me at my <a href='mailto:${answers.email}'>email</a> or you can visit my <a href="https://github.com/${answers.github}">GitHub Profile</a> for additional resources.\n\n`;
+    content += `## Questions\nFor questions please reach me at my [email](mailto:${answers.email}) or you can visit my [GitHub profile](https://github.com/${answers.github}) for additional resources.\n\n`;
 
-    
-
-
-
-
-
-    
-    
-    let buffer = new Buffer(content);
+    let buffer = new Buffer.from(content);
 
     // write the contents of the buffer, from position 0 to the end, to the file descriptor returned in opening our file
-    fs.write(fd, buffer, 0, buffer.length, null, function(err) {
-        if (err) throw 'error writing file: ' + err;
-        // fs.close(fd, function() {
-        //     console.log('wrote the file successfully');
-        // });
+    fs.write(file, buffer, 0, buffer.length, null, function (err) {
+      if (err) throw "Error writing file: " + err;
+      fs.close(file, function () {
+        console.log("Wrote the file successfully");
+      });
     });
-
-
   });
-
-  
-
-
-
-
-
-
-
-
+});
